bf459d36a8357c91372c89d1841a210c33daaa03
Amir-Ali-Nasrollahi <nasrollahiamirali@gmail.com>
2025-01-11 21:09:39 +0330

    solve question 3 with go :)

---
+++ q3/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func grep(pattern string, input []byte ) []string {
+	r, _ := regexp.Compile(pattern)
+
+	match := r.FindAllString(string(input), len(string(input)))
+
+	return match
+
+}
+func main() {
+	read, _ := os.ReadFile("./test.txt")
+	// fmt.Println(string(read))
+	match := grep(`mul\(\d+,\d+\)`, read)
+	var sum int 
+	var new_pattern string = `\d+`
+	for _, value := range match {
+
+		new_match := grep(new_pattern, []byte(value))
+		// fmt.Println(new_match[1])
+		firstItem, _ :=  strconv.Atoi(new_match[0])
+		secondItem, _ := strconv.Atoi(new_match[1])
+		
+		sum += firstItem * secondItem
+	}
+	fmt.Println(sum)
+}
